80ced97a4e9d0f856ad3f3c9201bb685e06c90c1
thdihan <thdihan@outlook.com>
2023-11-29 00:01:20 +0600

    Initial files in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 // Ui Testing Component Import
 import Navbar from "./UI_Testing/Navbar";
+import Button from "./UI_Testing/Button";
 const App = () => {
     return (
         <Router>
             <Routes>
                 {/* Ui Test Routes  */}
                 <Route path="/ui-test/navbar" element={<Navbar />} />
+                <Route path="/ui-test/button" element={<Button />} />
             </Routes>
         </Router>
     );
