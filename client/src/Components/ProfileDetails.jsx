732a1dc0a630d96ecfd82e5db7c40da4a031bc53
thdihan <thdihan@outlook.com>
2024-01-03 18:58:58 +0600

    Profile part basic structure done

--- client/src/Components/ProfileDetails.jsx
+++ client/src/Components/ProfileDetails.jsx
@@ -1,5 +1,6 @@
 import classes from "../Styles/ProfileDetails.module.css";
 import { LinkButton } from "./Buttons";
+import SingleQuestion from "./questions/SingleQuestion";
 
 const ProfileDetails = ({ rightSidebarState, setRightSidebarState }) => {
     return (
@@ -42,10 +43,39 @@ const ProfileDetails = ({ rightSidebarState, setRightSidebarState }) => {
                         />
                     </div> */}
                 </div>
-                <div></div>
             </div>
-            <div></div>
-            <div></div>
+            <div className={`${classes["personal-cards"]}`}>
+                <div
+                    className={`${classes["card"]} ${classes["question-answer"]}`}
+                >
+                    <div>
+                        <span>1</span>
+                        <span>Question</span>
+                    </div>
+                    <div>
+                        <span>0</span>
+                        <span>Answer</span>
+                    </div>
+                </div>
+                <div className={`${classes["card"]} ${classes["points"]}`}>
+                    <div>
+                        <span>0</span>
+                        <span>Points</span>
+                    </div>
+                </div>
+                <div className={`${classes["card"]} ${classes["badge"]}`}>
+                    <div>
+                        <span>0</span>
+                        <span>Badge</span>
+                    </div>
+                </div>
+            </div>
+            <div className={`${classes["questions"]}`}>
+                <h3>My Questions</h3>
+                <SingleQuestion />
+                <SingleQuestion />
+                <SingleQuestion />
+            </div>
         </div>
     );
 };
