5b22e6ba36ed770148ee962a6b91520a43ec7238
thdihan <thdihan@outlook.com>
2024-01-03 22:10:10 +0600

    Demo api : Profile

---
+++ demo-api.js
@@ -0,0 +1,24 @@
+const profile = {
+    fullname: "John Doe",
+    email: "username@gmail.com",
+    bio: "I am a fullstack developer",
+    favTags: ["nodejs", "reactjs", "mongodb", "python"],
+    badge: "Beginner",
+    points: 100,
+    follower: 2,
+    subscrition: {
+        status: true,
+        plan: "monthly",
+        expire: "2021-12-31",
+    },
+    expertUser: true,
+    following: {
+        count: 2,
+        users: [
+            { _id: "123", fullname: "John Doe" },
+            { _id: "123", fullname: "John Doe" },
+        ],
+    },
+};
+
+// Expert User means he has previledge to be an instructor.
