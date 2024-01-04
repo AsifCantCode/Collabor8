e7fab4076d50c963d88b8f500ece52c8a327ec66
thdihan <thdihan@outlook.com>
2024-01-04 11:11:16 +0600

    Bug fixed

--- client/src/Components/Signup.jsx
+++ client/src/Components/Signup.jsx
@@ -8,132 +8,140 @@ import classes from "../Styles/LoginSignup.module.css";
 import { Button } from "./Buttons";
 
 const Signup = ({ setLoginSignup }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [fullname, setFullname] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const { setUser } = useAuthContext();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [fullname, setFullname] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const { setUser } = useAuthContext();
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    toast.onChange((payload) => {
-      if (payload.status === "removed") {
-        navigate("/edit-profile");
-      }
-    });
-  }, [navigate]);
+    useEffect(() => {
+        toast.onChange((payload) => {
+            if (payload.status === "removed") {
+                navigate("/edit-profile");
+            }
+        });
+    }, [navigate]);
 
-  const handleSignup = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setError(false);
-    const fullname = e.target.fullname.value;
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-    const confirmPassword = e.target.confirmPassword.value;
-    console.log(fullname, email, password);
+    const handleSignup = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(false);
 
-    try {
-      const response = await UserApi.post(
-        "/signup",
-        { fullname, email, password, confirmPassword },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
+        try {
+            const response = await UserApi.post(
+                "/signup",
+                { fullname, email, password, confirmPassword },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            setLoading(false);
+            localStorage.setItem("user", JSON.stringify(response.data.token));
+            setUser(response.data.token);
+            toast.success(
+                "Signup Successful !! Navigating to edit profile page...",
+                {
+                    position: toast.POSITION.TOP_RIGHT,
+                    autoClose: 1500,
+                }
+            );
+            console.log("Signup Response Data : ", response.data);
+        } catch (err) {
+            console.log("SIGNUP ERROR: ", err);
+            setLoading(false);
+            setError(err.response.data.error);
         }
-      );
-      setLoading(false);
-      localStorage.setItem("user", JSON.stringify(response.data.token));
-      setUser(response.data.token);
-      toast.success("Signup Successful !! Navigating to edit profile page...", {
-        position: toast.POSITION.TOP_RIGHT,
-        autoClose: 1500,
-      });
-      console.log("Signup Response Data : ", response.data);
-    } catch (err) {
-      console.log("SIGNUP ERROR: ", err);
-      setLoading(false);
-      setError(err.response.data.error);
-    }
-  };
-  return (
-    <>
-      <form className={`${classes["signup-form"]}`} onSubmit={handleSignup}>
-        <div className={`${classes["input-field"]}`}>
-          <input
-            type="text"
-            name="fullname"
-            id=""
-            autoComplete="off"
-            value={fullname}
-            onChange={(e) => setFullname(e.target.value)}
-            required
-          />
-          <span className={fullname ? `${classes["valid"]}` : ""}>
-            Full Name
-          </span>
-        </div>
-        <div className={`${classes["input-field"]}`}>
-          <input
-            type="email"
-            name="email"
-            id=""
-            autoComplete="off"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <span className={email ? `${classes["valid"]}` : ""}>Email</span>
-        </div>
+    };
+    return (
+        <>
+            <form
+                className={`${classes["signup-form"]}`}
+                onSubmit={handleSignup}
+            >
+                <div className={`${classes["input-field"]}`}>
+                    <input
+                        type="text"
+                        name="fullname"
+                        id=""
+                        autoComplete="off"
+                        value={fullname}
+                        onChange={(e) => setFullname(e.target.value)}
+                        required
+                    />
+                    <span className={fullname ? `${classes["valid"]}` : ""}>
+                        Full Name
+                    </span>
+                </div>
+                <div className={`${classes["input-field"]}`}>
+                    <input
+                        type="email"
+                        name="email"
+                        id=""
+                        autoComplete="off"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <span className={email ? `${classes["valid"]}` : ""}>
+                        Email
+                    </span>
+                </div>
 
-        <div className={`${classes["input-field"]}`}>
-          <input
-            type="password"
-            name="password"
-            id=""
-            autoComplete="off"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <span className={password ? `${classes["valid"]}` : ""}>
-            Password
-          </span>
-        </div>
-        <div className={`${classes["input-field"]}`}>
-          <input
-            type="password"
-            name="confirmPassword"
-            id=""
-            autoComplete="off"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-          <span className={password ? `${classes["valid"]}` : ""}>
-            Confirm Password
-          </span>
-        </div>
-        {error && (
-          <p style={{ color: "red", paddingBottom: "15px" }}>{error}</p>
-        )}
-        <div>
-          <Button type="submit" text={`Signup`} />
-        </div>
-        <div>
-          <p className={`${classes["switch-form"]}`}>
-            Already have an account?{" "}
-            <span onClick={() => setLoginSignup("login")}> Login</span>
-          </p>
-        </div>
-      </form>
-      <ToastContainer position="top-right" />
-    </>
-  );
+                <div className={`${classes["input-field"]}`}>
+                    <input
+                        type="password"
+                        name="password"
+                        id=""
+                        autoComplete="off"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    <span className={password ? `${classes["valid"]}` : ""}>
+                        Password
+                    </span>
+                </div>
+                <div className={`${classes["input-field"]}`}>
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        id=""
+                        autoComplete="off"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                    />
+                    <span className={password ? `${classes["valid"]}` : ""}>
+                        Confirm Password
+                    </span>
+                </div>
+                {error && (
+                    <p style={{ color: "red", paddingBottom: "15px" }}>
+                        {error}
+                    </p>
+                )}
+                <div>
+                    <Button type="submit" text={`Signup`} />
+                </div>
+                <div>
+                    <p className={`${classes["switch-form"]}`}>
+                        Already have an account?{" "}
+                        <span onClick={() => setLoginSignup("login")}>
+                            {" "}
+                            Login
+                        </span>
+                    </p>
+                </div>
+            </form>
+            <ToastContainer position="top-right" />
+        </>
+    );
 };
 
 export default Signup;
--- client/src/Components/pages/AskQuestion.jsx
+++ client/src/Components/pages/AskQuestion.jsx
@@ -1,181 +1,241 @@
 import { useRef, useState } from "react";
 import classes from "../../Styles/AskQuestion.module.css";
-
+import UserApi from "../../apis/UserApi";
+import Jodit from "../Jodit";
+import JoditEditor from "jodit-react";
 import { Button } from "../Buttons";
-
+import { useAuthContext } from "../../Hooks/useAuthContext";
 // Icons
 import { ImCross } from "react-icons/im";
 import { LuImagePlus } from "react-icons/lu";
 import { RiDeleteBin5Fill } from "react-icons/ri";
-import { useAuthContext } from "../../Hooks/useAuthContext";
-import UserApi from "../../apis/UserApi";
-import Jodit from "../Jodit";
+
 const AskQuestion = () => {
-  // Text Input Section
+    // Text Input Section
+    const [textContent, setTextContent] = useState("");
 
-  const [textContent, setTextContent] = useState("");
+    // Image Input Section
+    const [selectedImage, setSelectedImage] = useState([]);
+    const [imageViewer, setImageViewer] = useState([]); // [false, true, false, false
+    const imageInputRef = useRef(null);
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
 
-  // Image Input Section
-  const [selectedImage, setSelectedImage] = useState([]);
-  const imageInputRef = useRef(null);
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
+        setSelectedImage([...selectedImage, file]);
+        console.log(file);
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImageViewer([...imageViewer, reader.result]);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
 
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setSelectedImage([...selectedImage, reader.result]);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
+    const handleImageClick = () => {
+        // Trigger the file input when the box is clicked
+        imageInputRef.current.click();
+    };
+    const handleDeleteImage = (index) => {
+        console.log(index);
+        const newImages = selectedImage.filter((image, i) => i !== index);
+        const newImageViewer = imageViewer.filter((image, i) => i !== index);
+        setImageViewer(newImageViewer);
+        setSelectedImage(newImages);
+    };
 
-  const handleImageClick = () => {
-    // Trigger the file input when the box is clicked
-    imageInputRef.current.click();
-  };
-  const handleDeleteImage = (index) => {
-    console.log(index);
-    const newImages = selectedImage.filter((image, i) => i !== index);
-    setSelectedImage(newImages);
-  };
+    // Tags Input Section
+    const [tagList, setTagList] = useState([]);
+    const [tag, setTag] = useState("");
 
-  // Tags Input Section
-  const [joditEditor, setJoditEditor] = useState(true);
-  const [tagList, setTagList] = useState([]);
-  const [tag, setTag] = useState("");
+    const handleTagChange = (event) => {
+        console.log("Tag Change");
+        event.stopPropagation();
+        const value = event.target.value;
+        // console.log(event.target.value);
+        if (value[value.length - 1] === ",") {
+            setTagList([...tagList, tag]);
+            setTag("");
+            console.log("Comma found");
+            console.log([...tagList, tag]);
+        } else {
+            setTag(event.target.value);
+        }
+    };
 
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const { user } = useAuthContext();
+    const handleTagDelete = (index) => {
+        const newTagList = tagList.filter((tag, i) => i !== index);
+        setTagList(newTagList);
+    };
 
-  const handleTagChange = (event) => {
-    console.log("Tag Change");
-    setJoditEditor(false);
-    event.stopPropagation();
-    const value = event.target.value;
-    // console.log(event.target.value);
-    if (value[value.length - 1] === ",") {
-      setTagList([...tagList, tag]);
-      setTag("");
-      console.log("Comma found");
-      console.log([...tagList, tag]);
-    } else {
-      setTag(event.target.value);
-    }
-  };
+    // Submit Section
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const { user } = useAuthContext();
+    const handleSubmit = async () => {
+        // e.preventDefault();
+        setLoading(true);
+        setError(false);
+        console.log("TEXT: ", textContent);
+        console.log("IMAGEL ", selectedImage);
+        console.log("TAG: ", tagList);
 
-  const handleTagDelete = (index) => {
-    const newTagList = tagList.filter((tag, i) => i !== index);
-    setTagList(newTagList);
-  };
+        const formData = new FormData();
+        formData.append("textContent", textContent);
+        formData.append("image", selectedImage);
+        formData.append("tagList", tagList);
 
-  const handleSubmit = async () => {
-    // e.preventDefault();
-    setLoading(true);
-    setError(false);
-    console.log("TEXT: ".textContent);
-    console.log("IMAGEL ", selectedImage);
-    console.log("TAG: ", tagList);
+        try {
+            const response = await UserApi.post("/add-question", formData, {
+                headers: {
+                    Authorization: `Bearer ${user}`,
+                },
+            });
+            setLoading(false);
 
-    const formData = new FormData();
-    formData.append("textContent", textContent);
-    formData.append("image", selectedImage);
-    formData.append("tagList", tagList);
+            //   toast.success("Signup Successful !! Navigating to edit profile page...", {
+            //     position: toast.POSITION.TOP_RIGHT,
+            //     autoClose: 1500,
+            //   });
+            console.log("ASK QUESTION Response: ", response.data);
+        } catch (err) {
+            console.log("ASK QUESTION ERROR: ", err);
+            setLoading(false);
+            setError(err.response.data.error);
+        }
+    };
 
-    try {
-      const response = await UserApi.post("/add-question", formData, {
-        headers: {
-          Authorization: `Bearer ${user}`,
-        },
-      });
-      setLoading(false);
+    // Jodit Editor Config
+    const editorConfig = {
+        readonly: false,
+        tabIndex: 1,
 
-      //   toast.success("Signup Successful !! Navigating to edit profile page...", {
-      //     position: toast.POSITION.TOP_RIGHT,
-      //     autoClose: 1500,
-      //   });
-      console.log("ASK QUESTION Response: ", response.data);
-    } catch (err) {
-      console.log("ASK QUESTION ERROR: ", err);
-      setLoading(false);
-      setError(err.response.data.error);
-    }
-  };
-  return (
-    <div className={`${classes["AskQuestion"]}`}>
-      <div className={`${classes["page-header"]}`}>
-        <h2>Ask a Question</h2>
-      </div>
+        askBeforePasteHTML: false,
+        askBeforePasteFromWord: false,
+        defaultActionOnPaste: "insert_clear_html",
 
-      <div className={`${classes["new-question"]}`}>
-        <Jodit textContent={textContent} setTextContent={setTextContent} />
-      </div>
+        placeholder: "Write something awesome ...",
+        beautyHTML: true,
+        toolbarButtonSize: "large",
+        buttons: [
+            "source",
+            "|",
+            "bold",
+            "italic",
+            "|",
+            "ul",
+            "ol",
+            "|",
+            "font",
+            "fontsize",
+            "paragraph",
+            "|",
+            "table",
+            "link",
+            "|",
+            "left",
+            "center",
+            "right",
+            "justify",
+            "|",
+            "undo",
+            "redo",
+            "|",
+            "fullsize",
+        ],
+        theme: "dark",
+    };
+    return (
+        <div className={`${classes["AskQuestion"]}`}>
+            <div className={`${classes["page-header"]}`}>
+                <h2>Ask a Question</h2>
+            </div>
 
-      <div className={`${classes["attachments"]}`}>
-        <p>Attachment Image (Optional)</p>
-        <div className={`${classes["images"]}`}>
-          {selectedImage &&
-            selectedImage.length > 0 &&
-            selectedImage.map((image, index) => (
-              <div
-                key={index}
-                className={classes.imageBox}
-                onClick={() => {
-                  handleDeleteImage(index);
-                }}
-              >
-                <span className={`${classes["delete-btn"]}`}>
-                  <RiDeleteBin5Fill />
-                </span>
-                <img src={image} alt="Selected" />
-              </div>
-            ))}
-          <div className={classes.imageBox} onClick={handleImageClick}>
-            <LuImagePlus />
-          </div>
-        </div>
-
-        <input
-          id="fileInput"
-          type="file"
-          accept="image/*"
-          onChange={handleImageChange}
-          style={{ display: "none" }}
-          ref={imageInputRef}
-        />
-      </div>
-
-      <div className={`${classes["tags"]}`}>
-        <label htmlFor="tags">Tags</label>
-        <div className={`${classes["tag-list"]}`}>
-          {tagList.length > 0 &&
-            tagList.map((tag, index) => (
-              <span key={index}>
-                {tag}{" "}
-                <ImCross
-                  onClick={() => {
-                    handleTagDelete(index);
-                  }}
+            {/* Text Box Section */}
+            <div className={`${classes["new-question"]}`}>
+                {/* <Jodit
+                    textContent={textContent}
+                    setTextContent={setTextContent}
+                /> */}
+                <JoditEditor
+                    config={editorConfig}
+                    value={textContent}
+                    tabIndex={1}
+                    onBlur={(newContent) => setTextContent(newContent)}
                 />
-              </span>
-            ))}
+            </div>
 
-          <input
-            type="text"
-            name="tag"
-            id=""
-            placeholder="Tag with comma"
-            value={tag}
-            onChange={handleTagChange}
-          />
+            {/* Image Attachment  */}
+            <div className={`${classes["attachments"]}`}>
+                <p>Attachment Image (Optional)</p>
+                <div className={`${classes["images"]}`}>
+                    {imageViewer &&
+                        imageViewer.length > 0 &&
+                        imageViewer.map((image, index) => (
+                            <div
+                                key={index}
+                                className={classes.imageBox}
+                                onClick={() => {
+                                    handleDeleteImage(index);
+                                }}
+                            >
+                                <span className={`${classes["delete-btn"]}`}>
+                                    <RiDeleteBin5Fill />
+                                </span>
+                                <img src={image} alt="Selected" />
+                            </div>
+                        ))}
+                    <div
+                        className={classes.imageBox}
+                        onClick={handleImageClick}
+                    >
+                        <LuImagePlus />
+                    </div>
+                </div>
+
+                <input
+                    id="fileInput"
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    style={{ display: "none" }}
+                    ref={imageInputRef}
+                />
+            </div>
+
+            {/* Tags Section  */}
+            <div className={`${classes["tags"]}`}>
+                <label htmlFor="tags">Tags</label>
+                <div className={`${classes["tag-list"]}`}>
+                    {tagList.length > 0 &&
+                        tagList.map((tag, index) => (
+                            <span key={index}>
+                                {tag}{" "}
+                                <ImCross
+                                    onClick={() => {
+                                        handleTagDelete(index);
+                                    }}
+                                />
+                            </span>
+                        ))}
+
+                    <input
+                        type="text"
+                        name="tag"
+                        id=""
+                        placeholder="Tag with comma"
+                        value={tag}
+                        onChange={handleTagChange}
+                    />
+                </div>
+            </div>
+
+            {/* Submit Button */}
+            <div className={`${classes["submit-btn"]}`}>
+                <Button func={handleSubmit} text="Confirm Post" />
+            </div>
         </div>
-      </div>
-      <div className={`${classes["submit-btn"]}`}>
-        <Button func={handleSubmit} text="Confirm Post" />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AskQuestion;
