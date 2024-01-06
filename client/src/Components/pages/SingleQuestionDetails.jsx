03eaccf300cc65e9a963939f2a1f425db9a762f9
thdihan <thdihan@outlook.com>
2024-01-07 00:34:02 +0600

    Answer Box done

--- client/src/Components/pages/SingleQuestionDetails.jsx
+++ client/src/Components/pages/SingleQuestionDetails.jsx
@@ -54,44 +54,6 @@ const SingleQuestionDetails = () => {
     const { id } = useParams();
     const { question, loading, error } = useGetSingleQuestion(id);
 
-    // {
-    //     "_id": "6599528d62e67fa34bbf4508",
-    //     "AuthorId": {
-    //         "subscription": {
-    //             "status": false
-    //         },
-    //         "badge": "newbie",
-    //         "points": 10,
-    //         "followers": 0,
-    //         "_id": "659635cee5a8a34b156eabdf",
-    //         "fullname": "Tanvir Hossain Dihan",
-    //         "email": "tanvirh.dihan@gmail.com",
-    //         "password": "$2b$10$fuX1RkUdk7mZSYbyYPcPCez3/.c46bJs1IP3bRDW.yTIPIxpzFGVG",
-    //         "favTags": [],
-    //         "following": [],
-    //         "__v": 0
-    //     },
-    //     "title": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quidem earum voluptas officia reprehenderit voluptates aperiam exercitationem ipsum quasi odit.",
-    //     "textContent": "<p>Contrary to popular belief, <strong>Lorem Ipsum</strong> is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. <strong><em>Richard McClintock</em></strong>, a Latin professor at <em>Hampden-Sydney College</em> in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.</p>\r\n<ul>\r\n    <li>ABCD&nbsp;</li>\r\n    <li>EFGS</li>\r\n</ul>\r\n<ol>\r\n    <li>ABCDDSD</li>\r\n    <li>SDG</li>\r\n</ol>\r\n<p>The standard chunk of <span style=\"font-family: georgia, palatino, serif;\">Lorem Ipsum used since the 1500s</span> is <span style=\"font-size: 30px;\">reproduced below</span> for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>\r\n<table style=\"border-collapse:collapse;width: 100%;\">\r\n    <tbody>\r\n        <tr>\r\n            <td style=\"width: 10%;\">sdasgsdfsdcds</td>\r\n            <td style=\"width: 10%;\">sdasgsdfsdcds</td>\r\n            <td style=\"width: 10%;\">sdasgsdfsdcds</td>\r\n            <td style=\"width: 10%;\">sdasgsdfsdcds</td>\r\n            <td style=\"width: 10%;\">sdasgsdfsdcds</td>\r\n            <td style=\"width: 10%;\">sdasgsdfsdcds</td>\r\n            <td style=\"width: 10%;\">sdasgsdfsdcds</td>\r\n            <td style=\"width: 10%;\">sdasgsdfsdcds</td>\r\n            <td style=\"width: 10%;\">sdasgsdfsdcds</td>\r\n            <td style=\"width: 10%;\">3sdfsdfs</td>\r\n        </tr>\r\n        <tr>\r\n            <td style=\"width: 10%;\">1</td>\r\n            <td style=\"width: 10%;\">2</td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n        </tr>\r\n        <tr>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n        </tr>\r\n        <tr>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n        </tr>\r\n        <tr>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n            <td style=\"width: 10%;\"><br></td>\r\n        </tr>\r\n    </tbody>\r\n    \r\n</table>\r\n<p><br></p>\r\n<p><a href=\"http://localhost:5173\">Home Page Link</a></p>",
-    //     "selectedImage": [
-    //         "images_1704546957815.png",
-    //         "images_1704546957817.png"
-    //     ],
-    //     "tagList": [
-    //         "React",
-    //         "MERN"
-    //     ],
-    //     "countUpVotes": 0,
-    //     "countDownVotes": 0,
-    //     "upVotes": [],
-    //     "downVotes": [],
-    //     "countAnswers": 0,
-    //     "isSolved": false,
-    //     "answers": [],
-    //     "postTime": "2024-01-06T13:15:57.838Z",
-    //     "updateTime": "2024-01-06T13:15:57.838Z",
-    //     "__v": 0
-    // }
     return (
         <div className={`${classes["SingleQuestionDetails"]}`}>
             <div className={`${classes["SingleQuestion"]}`}>
--- client/src/Components/questions/AnswerBox.jsx
+++ client/src/Components/questions/AnswerBox.jsx
@@ -1,6 +1,43 @@
 import classes from "../../Styles/AnswerBox.module.css";
 
+import { FaCircleUser } from "react-icons/fa6";
+import { FaThumbsUp } from "react-icons/fa";
+import { FaThumbsDown } from "react-icons/fa";
+import { useState } from "react";
+import textImage from "../../assets/Screenshot 2023-10-14 172945.png";
+import { SmallButton } from "../Buttons";
 const AnswerBox = () => {
+    const [upvote, setUpvote] = useState(0);
+    const [downvote, setDownvote] = useState(0);
+    const [upvoted, setUpvoted] = useState(false);
+    const [downvoted, setDownvoted] = useState(false);
+
+    const handleUpvote = () => {
+        if (upvoted) {
+            setUpvote(upvote - 1);
+            setUpvoted(false);
+        } else {
+            setUpvote(upvote + 1);
+            setUpvoted(true);
+        }
+        if (downvoted) {
+            setDownvote(downvote - 1);
+            setDownvoted(false);
+        }
+    };
+    const handleDownvote = () => {
+        if (downvoted) {
+            setDownvote(downvote - 1);
+            setDownvoted(false);
+        } else {
+            setDownvote(downvote + 1);
+            setDownvoted(true);
+        }
+        if (upvoted) {
+            setUpvote(upvote - 1);
+            setUpvoted(false);
+        }
+    };
     return (
         <div className={`${classes["AnswerBox"]}`}>
             <div className={`${classes["component-header"]}`}>
@@ -8,9 +45,123 @@ const AnswerBox = () => {
             </div>
             <div className={`${classes["answers"]}`}>
                 <div className={`${classes["answer"]}`}>
-                    <div className={`${classes["info"]}`}>
-                        <h4>Tanvir Hossain Dihan</h4>
-                        <p>Answered 2 days ago</p>
+                    <SmallButton text="Edit Answer" />
+                    <div className={`${classes["answer-header"]}`}>
+                        <div className={`${classes["info"]}`}>
+                            <h4 className={`${classes["answer-author"]}`}>
+                                <FaCircleUser />{" "}
+                                <span>Tanvir Hossain Dihan</span>
+                            </h4>
+                            <p className={`${classes["answer-date"]}`}>
+                                Answered 2 days ago
+                            </p>
+                        </div>
+                        <div className={`${classes["up-down-votes"]}`}>
+                            <p
+                                className={`${
+                                    upvoted ? classes["upvote"] : ""
+                                }`}
+                            >
+                                {upvote}
+                            </p>
+                            <FaThumbsUp
+                                className={`${
+                                    upvoted ? classes["upvote"] : ""
+                                }`}
+                                onClick={handleUpvote}
+                            />
+                            <FaThumbsDown
+                                className={`${
+                                    downvoted ? classes["downvote"] : ""
+                                }`}
+                                onClick={handleDownvote}
+                            />
+                            <p
+                                className={`${
+                                    downvoted ? classes["downvote"] : ""
+                                }`}
+                            >
+                                {downvote}
+                            </p>
+                        </div>
+                    </div>
+                    <div className={`${classes["answer-body"]}`}>
+                        <div className={`${classes["answer-text"]}`}>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Quisquam, voluptatum
+                                voluptates. Quibusdam, voluptatibus. Quae
+                                molestias, voluptates, quod, quia accusamus
+                                voluptatibus autem iusto voluptate quibusdam
+                                doloremque quos tempore? Repellat, voluptatum
+                                quibusdam.
+                            </p>
+                        </div>
+                        <div className={`${classes["answer-attachments"]}`}>
+                            <div className={`${classes["image"]}`}>
+                                <img src={textImage} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={`${classes["answer"]}`}>
+                    <SmallButton text="Edit Answer" />
+                    <div className={`${classes["answer-header"]}`}>
+                        <div className={`${classes["info"]}`}>
+                            <h4 className={`${classes["answer-author"]}`}>
+                                <FaCircleUser />{" "}
+                                <span>Tanvir Hossain Dihan</span>
+                            </h4>
+                            <p className={`${classes["answer-date"]}`}>
+                                Answered 2 days ago
+                            </p>
+                        </div>
+                        <div className={`${classes["up-down-votes"]}`}>
+                            <p
+                                className={`${
+                                    upvoted ? classes["upvote"] : ""
+                                }`}
+                            >
+                                {upvote}
+                            </p>
+                            <FaThumbsUp
+                                className={`${
+                                    upvoted ? classes["upvote"] : ""
+                                }`}
+                                onClick={handleUpvote}
+                            />
+                            <FaThumbsDown
+                                className={`${
+                                    downvoted ? classes["downvote"] : ""
+                                }`}
+                                onClick={handleDownvote}
+                            />
+                            <p
+                                className={`${
+                                    downvoted ? classes["downvote"] : ""
+                                }`}
+                            >
+                                {downvote}
+                            </p>
+                        </div>
+                    </div>
+                    <div className={`${classes["answer-body"]}`}>
+                        <div className={`${classes["answer-text"]}`}>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Quisquam, voluptatum
+                                voluptates. Quibusdam, voluptatibus. Quae
+                                molestias, voluptates, quod, quia accusamus
+                                voluptatibus autem iusto voluptate quibusdam
+                                doloremque quos tempore? Repellat, voluptatum
+                                quibusdam.
+                            </p>
+                        </div>
+                        <div className={`${classes["answer-attachments"]}`}>
+                            <div className={`${classes["image"]}`}>
+                                <img src={textImage} alt="" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
