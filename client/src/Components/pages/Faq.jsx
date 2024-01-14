aa8d3298bee6e5e235030a27edebaf1d50b86e3b
AsifCantCode <asif141100@gmail.com>
2024-01-14 16:46:17 +0600

    Update FAQ content

--- client/src/Components/pages/Faq.jsx
+++ client/src/Components/pages/Faq.jsx
@@ -11,55 +11,65 @@ import { Link } from "react-router-dom";
 const Faq = () => {
     const items = [
         {
-            heading: "What is Quinscape?",
+            heading: "What is Collabor8?",
             content:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit deserunt, cumque quae vitae blanditiis porro recusandae soluta, ullam illum molestiae autem, earum eveniet ut cupiditate atque! Inventore voluptatum, ipsa, dolores eum dolorem laborum aliquid animi suscipit incidunt ipsam in ad fugit reiciendis officiis perferendis fuga quia esse repellendus sed dolorum!",
+                "We’re glad you’re interested. Collabor8 is a platform where anyone can be a student in this diverse land of knowledge and anyone can be a mentor with the proper eligibility as well. Together we aim to bring the treasure of knowledge to a single platform, mostly for free.",
         },
         {
-            heading: "How do I ask a question?",
-            content:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quas totam amet mollitia eum deleniti, ullam dignissimos neque tenetur quisquam consectetur ea doloremque asperiores error id unde. Nisi minus repudiandae labore at molestias dolor commodi esse dignissimos consectetur ducimus odit est quo provident placeat fugit nemo, corporis voluptates, unde, quas perferendis consequatur saepe ratione sed. Odio delectus dolorem natus minima ad consectetur suscipit laboriosam facere voluptate magni corporis veritatis, hic possimus eum, explicabo rem voluptates cumque perferendis impedit odit ullam aliquid reprehenderit officia fuga. Explicabo inventore minus corrupti vel, nihil quidem facilis laboriosam rem officiis natus, asperiores quaerat aliquid fugiat.",
+            heading: "How does the Points system work?",
+            content: (
+                <div>
+                    <p>For the points, every user will be starting with 0 points and the points will be maintained in the following manners:</p>
+                    <ul style={{ listStyleType: 'disc' }}>
+                        <li>Questioning will add 1 point</li>
+                        <li>Answering will add 2 points</li>
+                        <li>Solved questions will add 5 points to the questioner and 10 points to the user giving the correct answer</li>
+                        <li>Each upvote of question/answer will add 2 points to the questioner or the answer-giver</li>
+                        <li>Each downvote will subtract 1 from the same</li>
+                    </ul>
+                </div>
+            ),
         },
         {
-            heading: "How do I answer a question?",
+            heading: "Want to be a part of this community? Join us!",
             content:
-                "You can answer a question by clicking on the 'Answer' button on the question card. You will be redirected to a page where you can answer the question.",
+                "Hurry up and jump into helping the community to gain more than 3000 points and ultimately being eligible for the \"Instructor\" posts' applications where you can earn money by giving services to the customers and also by making the community more knowledgeable by sharing your knowledge.",
         },
-        {
-            heading: "How do I edit my profile?",
-            content:
-                "You can edit your profile by clicking on the 'Edit Profile' button in the sidebar. You will be redirected to a page where you can edit your profile.",
-        },
-        {
-            heading: "How do I view my profile?",
-            content:
-                "You can view your profile by clicking on the 'Profile' button in the sidebar. You will be redirected to a page where you can view your profile.",
-        },
-        {
-            heading: "How do I view my questions?",
-            content:
-                "You can view your questions by clicking on the 'My Questions' button in the sidebar. You will be redirected to a page where you can view your questions.",
-        },
-        {
-            heading: "How do I view my collections?",
-            content:
-                "You can view your collections by clicking on the 'Collections' button in the sidebar. You will be redirected to a page where you can view your collections.",
-        },
-        {
-            heading: "How do I view all tags?",
-            content:
-                "You can view all tags by clicking on the 'Tags' button in the sidebar. You will be redirected to a page where you can view all tags.",
-        },
-        {
-            heading: "How do I view all questions?",
-            content:
-                "You can view all questions by clicking on the 'Home' button in the sidebar. You will be redirected to a page where you can view all questions.",
-        },
-        {
-            heading: "How do I view a question?",
-            content:
-                "You can view a question by clicking on the 'View Question' button on the question card. You will be redirected to a page where you can view the question.",
-        },
+        // {
+        //     heading: "How do I edit my profile?",
+        //     content:
+        //         "You can edit your profile by clicking on the 'Edit Profile' button in the sidebar. You will be redirected to a page where you can edit your profile.",
+        // },
+        // {
+        //     heading: "How do I view my profile?",
+        //     content:
+        //         "You can view your profile by clicking on the 'Profile' button in the sidebar. You will be redirected to a page where you can view your profile.",
+        // },
+        // {
+        //     heading: "How do I view my questions?",
+        //     content:
+        //         "You can view your questions by clicking on the 'My Questions' button in the sidebar. You will be redirected to a page where you can view your questions.",
+        // },
+        // {
+        //     heading: "How do I view my collections?",
+        //     content:
+        //         "You can view your collections by clicking on the 'Collections' button in the sidebar. You will be redirected to a page where you can view your collections.",
+        // },
+        // {
+        //     heading: "How do I view all tags?",
+        //     content:
+        //         "You can view all tags by clicking on the 'Tags' button in the sidebar. You will be redirected to a page where you can view all tags.",
+        // },
+        // {
+        //     heading: "How do I view all questions?",
+        //     content:
+        //         "You can view all questions by clicking on the 'Home' button in the sidebar. You will be redirected to a page where you can view all questions.",
+        // },
+        // {
+        //     heading: "How do I view a question?",
+        //     content:
+        //         "You can view a question by clicking on the 'View Question' button on the question card. You will be redirected to a page where you can view the question.",
+        // },
     ];
     return (
         <div className={`${classes["Faq"]}`}>
